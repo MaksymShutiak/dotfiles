19747c046b8cf937331c57472633ed7aa7510693
Maksym Shutiak <shutyak.maxym@gmail.com>
2022-12-20 20:51:43 +0200

    Updates

--- nvim/lua/maksym/plugins.lua
+++ nvim/lua/maksym/plugins.lua
@@ -50,7 +50,7 @@ return require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'L3MON4D3/LuaSnip' -- Snippets plugin
   -- LSP Autocompletition --
-  
+
   -- Code completition -- 
   use 'mattn/emmet-vim'
   use 'tpope/vim-surround'
