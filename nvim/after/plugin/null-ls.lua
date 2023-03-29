411d3edad45b4050e2dc70c89b1ddb3d4254fd66
Maksym Shutiak <shutyak.maxym@gmail.com>
2023-03-29 14:10:59 +0300

    Updates

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -2,5 +2,8 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
+      null_ls.builtins.diagnostics.cspell.with({
+        extra_args = { "--config", vim.fn.expand("~/.config/nvim/cspell.json") },
+      }),
     },
 })
